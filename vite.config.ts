80bff98426a7c2c9d50b819b6f7c63f859550785
nc204 <xydope@gmail.com>
2025-06-23 16:56:20 +0300

    chore(deps): add prettier and eslint configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const DEV_SERVER_PORT = 3000;
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+  server: {
+    port: DEV_SERVER_PORT,
+    // В dev-режиме пробрасываем запросы к /mockServiceWorker.js в папку src/mocks
+    proxy: {
+      "/mockServiceWorker.js": {
+        target: `http://localhost:${DEV_SERVER_PORT}`,
+        rewrite: () => path.resolve(__dirname, "src/mocks/mockServiceWorker.js"),
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-});
+}));
